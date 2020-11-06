73d1521c69f9ab63a382d14769461fa245dd69be
Glitch (hello-webpage) <none>
2020-11-06 21:00:21 +0000

    🐷💿 Checkpoint ./style(chill-setup).css:24728851/20 ./script.js:24728851/136 ./index(chill-setup).html:24728851/30 ./index(chill-result).html:24728851/3482 ./style(chill-result).css:24728851/2739

--- script.js
+++ script.js
@@ -3,18 +3,14 @@
 /* if user wants to add more text, add form*/
 let display_one = document.getElementById("display_one");
 let display_two = document.getElementById("display_two");
-let form_section = document.getElementById("form_section");
 let button_plus = document.getElementById("button_plus");
 
 // prints "hi" in the browser's dev tools console
-function formSection() {
-  let a = form_section;
-  a += 100;
-}
+
 
 // returns a new form section if user clicks plus_button
 function newForm() {
-  document.getElementById("form_section").style.height = "594px";
+  document.getElementById("form_section").style.height = "100vh";
 }
 console.log("b");
 
