ae1e42dbbc97a866f553f791b99677147da2db78
Glitch (hello-webpage) <none>
2020-11-06 06:33:44 +0000

    💺🚿 Checkpoint ./style(chill-setup).css:24728851/167 ./script.js:24728851/132 ./index(chill-setup).html:24728851/10

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 /* if user wants to add more text, add form*/
+let display_one = document.getElementById("display_one");
+let display_two = document.getElementById("display_two");
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
